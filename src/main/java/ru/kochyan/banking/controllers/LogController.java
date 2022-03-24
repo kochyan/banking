14bc6ac330395d248370cb6af7ef966cf8a611fc
Aleksey Gromov <lllalll7lblk@gmail.com>
2022-03-25 03:07:01 +0400

    create function to get log, order by id desc

--- src/main/java/ru/kochyan/banking/controllers/LogController.java
+++ src/main/java/ru/kochyan/banking/controllers/LogController.java
@@ -1,6 +1,8 @@
 package ru.kochyan.banking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
